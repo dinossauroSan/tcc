395b461493c9a594396743ed2b4c8afbee2559da
Sinval Henrique Ferreira Lima <sinvals.henrique.lima@gmail.com>
2025-04-03 06:47:11 -0300

    Update Main.java

--- tcc/src/tcc04/Main.java
+++ tcc/src/tcc04/Main.java
@@ -3,7 +3,17 @@ package tcc04;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Conjuntos conjuntos = new Conjuntos();
+        conjuntos.adicionarElementosA();
+        conjuntos.adicionarElementosB();
+        conjuntos.apresentarConjuntoA();
+        conjuntos.apresentarConjuntoB();
+        conjuntos.apresentarUniao();
+        conjuntos.apresentarIntersecao();
+        conjuntos.apresentarDiferenca();
+        conjuntos.apresentarProdutoCartesiano();
+        conjuntos.apresentarConjuntoDasPartesA();
+        conjuntos.apresentarConjuntoDasPartesB();
 	}
 
 }
